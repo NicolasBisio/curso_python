f1b45a1457831ce7daf261730937c4282af5097a
Nicolas Bisio <nb.bisio@gmail.com>
2023-11-24 11:46:13 -0300

    Improves in models

--- app_ecommerce/admin.py
+++ app_ecommerce/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from app_ecommerce.models import User, Product, Cart
 
 # Register your models here.
 admin.site.register(User)
--- app_ecommerce/models.py
+++ app_ecommerce/models.py
@@ -6,11 +6,20 @@ class User(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
 
+    def __str__(self):
+        return f"{self.last_name}, {self.name}"
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
+
 class Cart(models.Model):
     user = models.CharField(max_length=100)
     products = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"Cart of {self.user}"
