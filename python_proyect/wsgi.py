ed74104a9c6938fac5a25ba40102d5efb13d4225
Nicolas Bisio <nb.bisio@gmail.com>
2023-12-05 23:04:56 -0300

    Folders renamed

--- python_proyect/wsgi.py
+++ python_proyect/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for proyecto_python project.
+WSGI config for python_proyect project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_python.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_proyect.settings')
 
 application = get_wsgi_application()
