5f6a8058a3c7aa84ffe5f7be4403a8f2a1a9f79a
Nicolas Bisio <nb.bisio@gmail.com>
2023-12-11 03:53:08 -0300

    Update user

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+from accounts.views import user_register, user_login, user_update, user_update_avatar
+
+urlpatterns = [
+    path('register/', user_register),
+    path('login/', user_login),
+    path('logout/', LogoutView.as_view(template_name="accounts/logout.html")),
+    path('update/', user_update),
+    path('avatar/', user_update_avatar)
+    ]
---
+++ accounts/views.py
@@ -0,0 +1,94 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from accounts.forms import AvatarUpdateForm, UserRegisterForm, UserUpdateForm
+from accounts.models import Avatar
+
+# Login
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            username = data.get('username')
+            password = data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+
+        return redirect('/app/show_products/')
+
+    form = AuthenticationForm()
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/login.html", context)
+
+# Register
+def user_register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("/accounts/login/")
+
+    form = UserRegisterForm()
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/register.html", context)
+
+# Update
+@login_required
+def user_update(request):
+    user = request.user
+    if request.method == "POST":
+
+        form = UserUpdateForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user.email = data["email"]
+            user.last_name = data["last_name"]
+            user.save()
+            return redirect("/app/show_products/")
+
+    form = UserUpdateForm(initial={"email": user.email, "last_name": user.last_name})
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/register.html", context)
+
+@login_required
+def user_update_avatar(request):
+    user = request.user
+    if request.method == "POST":
+
+        form = AvatarUpdateForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            try:
+                avatar = user.avatar
+                avatar.imagen = data["imagen"]
+            except:
+                avatar = Avatar(
+                    user=user,
+                    imagen=data["imagen"]
+                )
+            avatar.save()
+
+            return redirect("/app/show_products/")
+
+    form = AvatarUpdateForm()
+    context = {
+        "form": form
+    }
+    return render(request, "accounts/avatar.html", context)
